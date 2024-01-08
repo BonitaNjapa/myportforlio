a4215986fbb4e616c3064ce8c716be45b7850c91
sixco <ntembekonjapa@hotmail.com>
2024-01-08 16:04:17 +0000

    added personal info table

--- src/portfolio.API/Database/PortfolioDbContext.cs
+++ src/portfolio.API/Database/PortfolioDbContext.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace portfolio.API.Database;
 
 using Microsoft.EntityFrameworkCore;
-
+using portfolio.API.Entities.PersonalInfo;
 
 public class PortfolioDbContext : DbContext
 {
     public PortfolioDbContext(DbContextOptions<PortfolioDbContext> options) : base(options) { }
+        public DbSet<PersonalInformation> PersonalInfos { get; set; }
+
 }
 
