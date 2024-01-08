19aa0758577e4c2fbddcd3f0b6fde627ef726683
sixco <ntembekonjapa@hotmail.com>
2024-01-08 18:01:43 +0000

    changed date to string

--- src/portfolio.API/Entities/PersonalInfo/PersonalInformation.cs
+++ src/portfolio.API/Entities/PersonalInfo/PersonalInformation.cs
@@ -4,13 +4,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace portfolio.API.Entities.PersonalInfo;
 
 [Table(name: "personal_information")]
-public class PersonalInformation
+public class Person
 {
     [Key]
     public int personal_info_id { get; set; }
     public required string first_name { get; set; }
     public required string last_name { get; set; }
-    public DateTime date_of_birth { get; set; }
+    public required string date_of_birth { get; set; }
     public required string gender { get; set; }
     public required string ethnicity { get; set; }
 }
