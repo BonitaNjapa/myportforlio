f34af05305d2e516558d9e92a46be93be414f996
sixco <ntembekonjapa@hotmail.com>
2024-01-08 13:27:32 +0000

    add db to program as a service

--- src/portfolio.API/Program.cs
+++ src/portfolio.API/Program.cs
@@ -1,12 +1,23 @@
 using FastEndpoints;
-
+using portfolio.API.Database;
+using Microsoft.EntityFrameworkCore;
+using portfolio.API.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+var Configuration = builder.Configuration;
+
+var dbSettings = Configuration.GetSection("ConnectionStrings:DefaultConnection").Get<DatabaseSettings>();
+
+builder.Services.AddDbContext<PortfolioDbContext>(options =>
+        options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddFastEndpoints();
 
 var app = builder.Build();
 
+app.CheckDatabaseConnection();
 app.UseFastEndpoints();
 
 app.MapGet("/", () => "Hello World!");
