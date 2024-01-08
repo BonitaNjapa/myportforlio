a8df00cf571cc416332a92fb808de3be24fae69b
sixco <ntembekonjapa@hotmail.com>
2024-01-08 18:01:03 +0000

    mapped carter

--- src/portfolio.API/Program.cs
+++ src/portfolio.API/Program.cs
@@ -1,3 +1,4 @@
+using Carter;
 using portfolio.API.Helpers.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,11 +9,13 @@ Configuration.AddEnvironmentBasedJsonFile(builder.Environment);
 
 builder.Services.AddCustomDbContext(configuration:Configuration);
 
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
 var app = builder.Build();
 
 app.CheckDatabaseConnection();
 
+app.MapCarter();
 
 app.MapGet("/", () => "Hello World!");
 
