ddf0f6a77eba283744984ce789c9240f76bed28a
sixco <ntembekonjapa@hotmail.com>
2024-01-08 16:38:52 +0000

    removed fastEndpoints

--- src/portfolio.API/Program.cs
+++ src/portfolio.API/Program.cs
@@ -1,4 +1,3 @@
-using FastEndpoints;
 using portfolio.API.Helpers.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,13 +8,11 @@ Configuration.AddEnvironmentBasedJsonFile(builder.Environment);
 
 builder.Services.AddCustomDbContext(configuration:Configuration);
 
-builder.Services.AddFastEndpoints();
 
 var app = builder.Build();
 
 app.CheckDatabaseConnection();
 
-app.UseFastEndpoints();
 
 app.MapGet("/", () => "Hello World!");
 
