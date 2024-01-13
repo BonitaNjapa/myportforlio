568f4ff3e70f256dff53df8aaaeb184081d3171d
sixco <ntembekonjapa@hotmail.com>
2024-01-13 19:54:34 +0000

    added all routes for newly created components

--- src/client/clientapp/src/app/app.routes.ts
+++ src/client/clientapp/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from '../Components/Register/register/register.component';
+import { LoginComponent } from '../Components/Login/login/login.component';
+import { HomeComponent } from '../Components/home/home.component'; 
+import { PageNotFoundComponent } from '../Components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+
+
+
+export const routes: Routes = [
+
+    { path: "", component: HomeComponent },
+    { path: "Register", component: RegisterComponent },
+    { path: "Login", component: LoginComponent },
+     { path: '**', component: PageNotFoundComponent },
+];
