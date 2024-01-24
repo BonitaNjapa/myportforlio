5a93edcaa89e44ac3c9b871d423dc8b319341f84
sixco <ntembekonjapa@hotmail.com>
2024-01-24 21:23:02 +0000

    setup landing page

--- src/portfolio.API/ClientApp/src/app/components/dashboard/dashboard.component.ts
+++ src/portfolio.API/ClientApp/src/app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 
+interface Stat
+{
+  name: string;
+  value: string;
+  icon: string;
+  iconBg: string;
+}
+
 
 @Component({
   selector: 'app-dashboard',
@@ -11,8 +19,17 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  constructor(private http: HttpClient) {
-  }  
   
- 
+  stats : Stat[] = [];
+
+  constructor(private http: HttpClient) {
+    const initStats = 
+    [
+        { name: 'Budget',      iconBg: "icon icon-shape bg-danger text-white text-lg rounded-circle", icon: "bi bi-credit-card",    value: '$750.90' },
+        { name: 'New projects',iconBg: "icon icon-shape bg-primary text-white text-lg rounded-circle",icon: "bi bi-people",         value: '215' },
+        { name: 'Total hours', iconBg: "icon icon-shape bg-info text-white text-lg rounded-circle",   icon: "bi bi-clock-history",  value: '1.400' },
+        { name: 'Work load',   iconBg: "icon icon-shape bg-warning text-white text-lg rounded-circle",icon: "bi bi-minecart-loaded",value: '95%' },
+    ];
+    this.stats = initStats
+  }  
 }
